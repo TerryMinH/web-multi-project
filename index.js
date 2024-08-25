8939b1bc6bc271e96fa6c3413aa78b18f57fc983
huangminguang <17621481287@163.com>
2024-08-25 16:48:06 +0800

    mark: Vue feature: reg

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const arrayLike = {
-  length: 3,
-  unrelated: "foo",
-  0: 5,
-  2: 4,
-  3: 3, // 由于 length 属性的值为 3，with() 会忽略该值
-};
-console.log(typeof module.exports);
+/*
+ * @Author: TerryMin
+ * @Date: 2024-07-29 17:58:09
+ * @LastEditors: TerryMin
+ * @LastEditTime: 2024-08-25 16:30:41
+ * @Description: file not
+ */
+
+const callbackUrl ="http://36.155.98.130/mgyy/site/prd/index.html";
+const WHITE_RULES = [/^https?:\/\/movie\.miguvideo\.com$/, /^https?:\/\/36.155.98.130$/,/^https?:\/\/36\.155\.98\.130\/mgyy\/.*$/];
+
+const isLegal = WHITE_RULES.some((rule) => {
+  try {
+    const originName = new URL(callbackUrl).origin;
+    return rule.test(originName);
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+});
+// console.log('isLegal==>',isLegal);
+
+const str='aabbcc';
+const result=str.replace(/aa/g, 'DD');
+console.log(result);
