f503606f921411e36c2bda4014ffb3e63654b4c3
huangminguang <17621481287@163.com>
2024-09-19 17:06:22 +0800

    mark: Vue feature: add react project

---
+++ react-project/my-app/src/App.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: TerryMin
+ * @Date: 2024-09-19 16:52:15
+ * @LastEditors: TerryMin
+ * @LastEditTime: 2024-09-19 17:03:41
+ * @Description: file not
+ */
+import logo from "./logo.svg";
+import "./App.css";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
