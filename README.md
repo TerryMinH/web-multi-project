9b988a24cc05ab2a6581024ac785a31d5f687168
huangminguang <17621481287@163.com>
2024-10-23 11:33:44 +0800

    mark: Vue feature: delete 1

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
  * @Author: TerryMin
  * @Date: 2024-06-09 10:36:36
  * @LastEditors: TerryMin
- * @LastEditTime: 2024-06-09 11:07:27
+ * @LastEditTime: 2024-10-23 11:33:20
  * @Description: file not
 -->
 # web-multi-project
-个人代码学习及知识记录1
+个人代码学习及知识记录
--- index.js
+++ index.js
@@ -2,25 +2,10 @@
  * @Author: TerryMin
  * @Date: 2024-07-29 17:58:09
  * @LastEditors: TerryMin
- * @LastEditTime: 2024-09-13 17:37:36
+ * @LastEditTime: 2024-10-11 10:37:46
  * @Description: file not
  */
-
-function hasKey(obj, keys) {
-  var o = obj;
-  keys.slice(0, -1).forEach(function (key) {
-    o = o[key] || {};
-  });
-  console.log(o);
-  var key = keys[keys.length - 1];
-  return key in o;
-}
-
-const obj1 = {
-  name: `terry`,
-  age: 18,
-  address: `beijing`,
-};
-const arr1 = ["name", "age", "address"];
-
-console.log(hasKey(obj1, arr1));
+const a = null;
+const b = 0;
+const result = a?? b;
+console.log(result); // 输出 10
