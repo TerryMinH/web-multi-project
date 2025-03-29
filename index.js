979514f70489e710e3ed4431feb01052950b2364
huangminguang <15896938825@163.com>
2025-03-29 20:31:32 +0800

    mark: default add feature: arithmetic

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-class Calculator {
-  constructor() {
-    this.rule = new Map();
+let arr = [2, 9, 6, 7, 4, 3, 1, 7, 0, -1, -2];
+
+function bubbleSort(arr) {
+  let len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
   }
-  registerRule(name, fn) {
-    this.rule.set(name, fn);
-  }
-  revoke(name, ...args) {
-    return this.rule.get(name)(args);
-  }
+  return arr;
 }
+
+function quickSort(arr) {
+  console.log(arr);
+  if (arr.length === 0) {
+    return [];
+  }
+  let mid = Math.floor(arr.length / 2);
+  let current = arr.splice(mid, 1);
+  let left = [],
+    right = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < current) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return quickSort(left).concat(current, quickSort(right));
+}
+console.log(quickSort(arr));
